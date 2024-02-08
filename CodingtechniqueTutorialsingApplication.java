a5130b4c9413221c7b924e421f86d49e5b232e1e
Vamshi Krishna Lingampelly <43585723+vamshijohnlee456@users.noreply.github.com>
2024-02-08 20:12:33 +0530

    added using drag files directly to git

---
+++ CodingtechniqueTutorialsingApplication.java
@@ -0,0 +1,16 @@
+package admin_user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
+public class CodingtechniqueTutorialsingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodingtechniqueTutorialsingApplication.class, args);
+	}
+
+}
